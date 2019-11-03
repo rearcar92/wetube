d1abef4d24f52fede6fbfee706dfef318b12505e
rearcar92 <rearcar92@gmail.com>
2019-11-03 23:19:05 +0900

    routes

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,14 @@
 import express from  "express";
+import routes from "../routes";
+import { videos, upload, videosDetail, editVideo, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videosDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 
 
