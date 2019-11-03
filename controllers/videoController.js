2d5e27964367b87ec9c7c77147fde201ca85e9a5
rearcar92 <rearcar92@gmail.com>
2019-11-03 23:19:52 +0900

    controller

---
+++ controllers/videoController.js
@@ -0,0 +1,7 @@
+export const home = (req, res) => res.send("home");
+export const search = (req, res) => res.send("search");
+export const videos = (req, res) => res.send("videos");
+export const upload = (req, res) => res.send("upload");
+export const videosDetail = (req, res) => res.send("videosDetail");
+export const editVideo = (req, res) => res.send("editVideos");
+export const deleteVideo = (req, res) => res.send("deleteVideo");
