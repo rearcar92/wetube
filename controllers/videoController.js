7325a68d7cc314c95931890072f06cf9d7dd87de
rearcar92 <rearcar92@gmail.com>
2019-11-06 02:06:32 +0900

    search req.query

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,11 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home"});
-export const search = (req, res) => res.render("search", { pageTitle: "Search"});
+export const search = (req, res) => {
+    const { 
+        query: { term : searchingBy }
+    } = req;
+    // const searchingBy = req.query.term;
+    res.render("search", { pageTitle: "Search", searchingBy});
+}
 export const videos = (req, res) => res.render("videos", { pageTitle: "Videos"});
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload"});
 export const videosDetail = (req, res) => res.render("videosDetail", { pageTitle: "Videos Detail"});
