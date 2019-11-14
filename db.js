0be4fbf2f3175e50313b34b889dead526f2898a4
rearcar92 <rearcar92@gmail.com>
2019-11-15 02:22:26 +0900

    mongodb

--- db.js
+++ db.js
@@ -1,50 +1,20 @@
-export const videos = [
-    {
-      id: 324393,
-      title: "Video awesome",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 1212121,
-      title: "Video super",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 55555,
-      title: "Video nice",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 11111,
-      title: "Video perfect",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    }
-  ];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connted to DB")
+const handleError = (error) => console.log(`Error on DB conntion:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
