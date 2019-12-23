0334a4249dbdb24b172c4705fcc575262669272e
rearcar92 <rearcar92@gmail.com>
2019-12-24 01:33:55 +0900

    WeTube

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-
 import express from "express";
 // const express = require("express");
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { MiddleWare } from "./middleWare";
+import {
+    MiddleWare
+} from "./middleWare";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -16,9 +17,12 @@ const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(morgan("dev"));
 app.use(MiddleWare);
 
